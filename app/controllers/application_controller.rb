110ff7c9cf2688f9bc37954471045fb9d98e8ba8
Jon McCarthy <jtmcc93@gmail.com>
2017-01-11 10:44:28 -0700

    add items by category view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :categories, :brands
+
+  def categories
+    @categories = Category.all
+  end
+
+  def brands
+    @brands =Product.pluck(:brand).sort.uniq!
+
+    end
+  end
+
 end
--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,9 +1,14 @@
 class StorefrontController < ApplicationController
+  before_action :authenticate_user!
+
+
   def all_items
     @products = Product.all
   end
 
   def items_by_category
+    @products = Product.where(category: params[:cat_id])
+    @category = Category.find(params[:cat_id])
   end
 
   def items_by_brand
