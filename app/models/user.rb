a0cf6e37aa7338662382fd16e4df800da9759af0
Jon McCarthy <jtmcc93@gmail.com>
2017-01-11 12:19:20 -0700

    add user roles and admin abilities

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
          has_many :orders
+         def admin?
+           roll == "admin"
+         end
 end
