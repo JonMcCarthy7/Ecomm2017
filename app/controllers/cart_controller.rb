ae45c19f91fdef1093339f1da94a2c7eecc8d40f
Jon McCarthy <jtmcc93@gmail.com>
2017-01-13 11:45:51 -0700

    add badge to cart in navbar

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,60 +1,59 @@
 class CartController < ApplicationController
 
-  before_filter :authenticate_user!, :except => [:add_to_cart, :view_order]
+   before_filter :authenticate_user!, :except => [:add_to_cart, :view_order]
 
-  def add_to_cart
-    line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity])
 
-    line_item.line_item_total = line_item.quantity * line_item.product.price
-    line_item.save
+   def add_to_cart
+     line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity])
 
-    redirect_to root_path
-  end
+     line_item.line_item_total = 1 * line_item.product.price
+     line_item.save
 
-  def view_order
-    @line_items = LineItem.all
-  end
+     redirect_to root_path
+   end
 
-  def checkout
-    @line_items = LineItem.all
-    @order = Order.create(user_id: current_user.id, subtotal: 0)
+ def view_order
+     @line_items = LineItem.all
+     @order = Order.create(user_id: current_user.id, subtotal: 0)
 
-    @line_items.each do |line_item|
-      @order.order_items[line_item.product_id] = line_item.quantity
-      @order.subtotal += line_item.line_item_total
-    end
+     @line_items.each do |line_item|
+     @order.order_items[line_item.product_id] = line_item.quantity
+     @order.subtotal += line_item.line_item_total
+ end
 
     @order.sales_tax = @order.subtotal * 0.07
     @order.grand_total = @order.subtotal + @order.sales_tax
     @order.save
 
-    @line_items.each do |line_item|
-      line_item.product.quantity -= line_item.quantity
-      line_item.product.save
-    end
+ @line_items.each do |line_item|
+ line_item.quantity
+ line_item.product.save
+end
+ LineItem.destroy_all
+end
 
-    LineItem.destroy_all
-  end
 
-  def order_complete
-    @order = Order.find(params[:order_id])
-    @amount = (@order.grand_total.to_f.round(2) * 100).to_i
+def order_complete
+ @order = Order.find(params[:order_id])
+   @amount = (@order.grand_total.to_f.round(2) * 100).to_i
 
-    customer = Stripe::Customer.create(
-      :email => current_user.email,
-      :card => params[:stripeToken]
-    )
+   customer = Stripe::Customer.create(
+     :email => current_user.email,
+     :card => params[:stripeToken]
+   )
 
-    charge = Stripe::Charge.create(
-      :customer => customer.id,
-      :amount => @amount,
-      :description => 'Rails Stripe customer',
-      :currency => 'usd'
-    )
+   charge = Stripe::Charge.create(
+     :customer => customer.id,
+     :amount => @amount,
+     :description => 'Rails Stripe customer',
+     :currency => 'usd'
+   )
 
-    rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to charges_path
-  end
+   rescue Stripe::CardError => e
+   flash[:error] = e.message
+   redirect_to charges_path
+
+
+end
 
 end
